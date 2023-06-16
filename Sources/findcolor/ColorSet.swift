068bb4ddc4fcb8a0339d8efc4fa856edda7d1330
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-06-17 02:59:12 +0300

    Add color set structure and test

---
+++ Sources/findcolor/ColorSet.swift
@@ -0,0 +1,63 @@
+//
+//  ColorSet.swift
+//  
+//
+//  Created by Artem Yelizarov on 17.06.2023.
+//
+
+import Foundation
+
+struct ColorSet: Decodable, Equatable {
+    struct Info: Decodable, Equatable {
+        var author: String
+        var version: Double
+    }
+
+    var colors: [ColorAsset]
+    var info: Info
+}
+
+struct ColorAsset: Decodable, Equatable {
+    struct Color: Decodable, Equatable {
+        enum ColorSpace: String, Decodable {
+            case srgb
+            case extendedSrgb = "extended-srgb"
+            case extendedLinearSrgb = "extended-linear-srgb"
+        }
+
+        struct Components: Decodable, Equatable {
+            var alpha: String
+            var blue: String
+            var green: String
+            var red: String
+        }
+
+        var colorSpace: ColorSpace
+        var components: Components
+
+        enum CodingKeys: String, CodingKey {
+            case components
+            case colorSpace = "color-space"
+        }
+    }
+
+    enum Idiom: String, Decodable {
+        case universal
+        case iPhone = "iphone"
+        case iPad = "ipad"
+        case car
+        case mac
+        case watch
+        case tv
+    }
+
+    struct Appearance: Decodable, Equatable {
+        var appearance: String
+        var value: String
+    }
+
+    var color: Color
+    var idiom: Idiom
+    var appearances: [Appearance]?
+    var subtype: String?
+}
--- Tests/findcolorTests/findcolorTests.swift
+++ Tests/findcolorTests/findcolorTests.swift
@@ -1,8 +1,61 @@
+import Foundation
 import XCTest
 @testable import findcolor
 
 final class findcolorTests: XCTestCase {
-    func test_helloWorld() throws {
-        XCTAssertEqual(findcolor().text, "Hello, World!")
+    func test_decodingColorSet_withSingleColor_succeeds() throws {
+        // Given
+        let whiteColorString = """
+                          {
+                            "colors" : [
+                              {
+                                "color" : {
+                                  "color-space" : "srgb",
+                                  "components" : {
+                                    "alpha" : "1.000",
+                                    "blue" : "0xFF",
+                                    "green" : "0xFF",
+                                    "red" : "0xFF"
+                                  }
+                                },
+                                "idiom" : "universal"
+                              }
+                            ],
+                            "info" : {
+                              "author" : "xcode",
+                              "version" : 1
+                            }
+                          }
+                          """
+        let colorData = try XCTUnwrap(whiteColorString.data(using: .utf8))
+
+        // When
+        let colorSet: ColorSet = try JSONDecoder().decode(
+            ColorSet.self,
+            from: colorData
+        )
+
+        // Then
+        let expectedColorSet = ColorSet(
+            colors: [
+                .init(
+                    color: .init(
+                        colorSpace: .srgb,
+                        components: .init(
+                            alpha: "1.000",
+                            blue: "0xFF",
+                            green: "0xFF",
+                            red: "0xFF"
+                        )
+                    ),
+                    idiom: .universal
+                )
+            ],
+            info: .init(
+                author: "xcode",
+                version: 1
+            )
+        )
+        XCTAssertEqual(colorSet, expectedColorSet)
     }
 }
