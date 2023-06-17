ffff2e14934933d721a20c4072a8b3f3ee779575
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-06-17 18:40:38 +0300

    Add tests for finding trimmed hex

--- Tests/hexcodeTests/ColorFinderTests.swift
+++ Tests/hexcodeTests/ColorFinderTests.swift
@@ -11,6 +11,8 @@ import XCTest
 final class ColorFinderTests: XCTestCase {
     typealias SUT = ColorFinder
 
+    // MARK: - Test find hex with hash
+
     func test_findHexWithHash_inColorSetsWithSingleExpectedColor_findsExpectedColor() {
         // Given
         let sut = SUT()
@@ -39,6 +41,41 @@ final class ColorFinderTests: XCTestCase {
 
         // When
         let colors = sut.find("#000000", in: [.whiteUniversalSingularColorSet])
+
+        // Then
+        XCTAssert(colors.isEmpty)
+    }
+
+    // MARK: - Test find trimmed hex
+
+    func test_findTrimmedHex_inColorSetsWithSingleExpectedColor_findsExpectedColor() {
+        // Given
+        let sut = SUT()
+
+        // When
+        let colors = sut.find("FFFFFF", in: [.whiteUniversalSingularColorSet])
+
+        // Then
+        XCTAssertEqual(colors, [.whiteUniversalSingularColorSet])
+    }
+
+    func test_findTrimmedHex_inEmptyArray_findsNothing() {
+        // Given
+        let sut = SUT()
+
+        // When
+        let colors = sut.find("FFFFFF", in: [])
+
+        // Then
+        XCTAssert(colors.isEmpty)
+    }
+
+    func test_findTrimmedHex_inColorSetsWithoutExpectedColor_findsNothing() {
+        // Given
+        let sut = SUT()
+
+        // When
+        let colors = sut.find("000000", in: [.whiteUniversalSingularColorSet])
 
         // Then
         XCTAssert(colors.isEmpty)
