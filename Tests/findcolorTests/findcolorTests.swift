4f16c6ea2377363c901514a8f59f39de17e374cf
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-06-17 03:12:21 +0300

    Add test to decode from file

--- Tests/findcolorTests/findcolorTests.swift
+++ Tests/findcolorTests/findcolorTests.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import findcolor
 
 final class findcolorTests: XCTestCase {
-    func test_decodingColorSet_withSingleColor_succeeds() throws {
+    func test_decodeColorSet_withSingleColor_succeeds() throws {
         // Given
         let whiteColorString = """
                           {
@@ -28,6 +28,46 @@ final class findcolorTests: XCTestCase {
                           }
                           """
         let colorData = try XCTUnwrap(whiteColorString.data(using: .utf8))
+
+        // When
+        let colorSet: ColorSet = try JSONDecoder().decode(
+            ColorSet.self,
+            from: colorData
+        )
+
+        // Then
+        let expectedColorSet = ColorSet(
+            colors: [
+                .init(
+                    color: .init(
+                        colorSpace: .srgb,
+                        components: .init(
+                            alpha: "1.000",
+                            blue: "0xFF",
+                            green: "0xFF",
+                            red: "0xFF"
+                        )
+                    ),
+                    idiom: .universal
+                )
+            ],
+            info: .init(
+                author: "xcode",
+                version: 1
+            )
+        )
+        XCTAssertEqual(colorSet, expectedColorSet)
+    }
+
+    func decodeColorSet_fromFile_succeeds() throws {
+        // Given
+        let filePath = try XCTUnwrap(
+            Bundle.main.url(
+                forResource: "Assets.xcassets/whiteColor.colorset/Contents",
+                withExtension: "json"
+            )
+        )
+        let colorData = try Data(contentsOf: filePath)
 
         // When
         let colorSet: ColorSet = try JSONDecoder().decode(
