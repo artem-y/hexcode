61d237dde63c03d46fa0b7e4416316de1500a9e3
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-11-20 11:57:22 +0200

    Refactor RGB conversion logic and slightly improve performance (#14)
    * Remove extra check to speed up rgb to string convertion
    * Update gitignore to exclude whole spm folder
    * Reorganize rgb string conversion code to clean up duplications

--- Sources/hexcode/Models/ColorAsset.swift
+++ Sources/hexcode/Models/ColorAsset.swift
@@ -45,32 +45,10 @@ extension ColorAsset {
                 components.blue
             ]
 
-            if rgb
-                .filter(isValidHexComponent)
-                .count == 3 {
-                return rgb
-                    .reduce("", +)
-                    .replacingOccurrences(of: "0x", with: "")
-            }
-
-            if rgb
-                .filter(isValidFloatComponent)
-                .count == 3 {
-
-                let floatComponents = rgb
-                    .compactMap(convertFloatToHexadecimal)
-                if floatComponents.count == 3 {
-                    return floatComponents
-                        .reduce("", +)
-                }
-            }
-
-            let hexComponents = rgb
-                .compactMap(convertIntToHexadecimal)
-
-            guard hexComponents.count == 3 else { return "" }
-            return hexComponents
-                .reduce("", +)
+            return convert(rgb, from: .hex)
+            ?? convert(rgb, from: .int)
+            ?? convert(rgb, from: .float)
+            ?? ""
         }
 
         enum CodingKeys: String, CodingKey {
@@ -81,12 +59,26 @@ extension ColorAsset {
 }
 
 extension ColorAsset.Color {
-    private func isValidHexComponent(_ component: String) -> Bool {
-        matchRegex(component, pattern: "0x[0-9a-fA-F]{2}")
+    private func convert(_ components: [String], from rawComponentType: ComponentType) -> String? {
+        let converter = switch rawComponentType {
+        case .float: convertFloatToHexadecimal
+        case .int: convertIntToHexadecimal
+        case .hex: convertRawToHexadecimal
+        }
+
+        let hexComponents = components.compactMap(converter)
+        guard hexComponents.count == 3 else { return nil }
+
+        return hexComponents.joined()
     }
 
-    private func isValidFloatComponent(_ component: String) -> Bool {
-        matchRegex(component, pattern: "[0-1]\\.[0-9]+")
+    private func convertRawToHexadecimal(_ component: String) -> String? {
+        guard isValidHexComponent(component) else { return nil }
+        return String(component.dropFirst(2))
+    }
+
+    private func isValidHexComponent(_ component: String) -> Bool {
+        matchRegex(component, pattern: "0x[0-9a-fA-F]{2}")
     }
 
     private func matchRegex(_ string: String, pattern: String) -> Bool {
@@ -142,5 +134,15 @@ extension ColorAsset.Color {
         var red: String
         var green: String
         var blue: String
+    }
+}
+
+// MARK: - ColorAsset.Color.ComponentType
+
+extension ColorAsset.Color {
+    private enum ComponentType {
+        case float
+        case hex
+        case int
     }
 }
