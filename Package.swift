a1d5a58c4d18f446d3dee36d36433caf77afd827
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2024-01-06 12:41:48 +0200

    Bump swifty-test-assertions version to 0.1.1.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(url: "https://github.com/artem-y/swifty-test-assertions.git", from: "0.1.0"),
+        .package(url: "https://github.com/artem-y/swifty-test-assertions.git", from: "0.1.1"),
     ],
     targets: [
         .executableTarget(
