09fffc8d42eb41035dd954c390b3188b368b211c
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2024-04-25 02:14:04 +0300

    Fall back to macos 13 for wider support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "hexcode",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v13),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
